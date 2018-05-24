b8680e67620c056e16f6040294a6bcad500c7612
kennywangs <wangke414@163.com>
2018-05-24 16:40:47 +0800

    jsonp support

--- src/main/java/com/xxb/BootTestApplication.java
+++ src/main/java/com/xxb/BootTestApplication.java
@@ -1,9 +1,16 @@
 package com.xxb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.xxb" })
+@EnableAutoConfiguration(exclude = { MultipartAutoConfiguration.class })
+@EnableTransactionManagement
 public class BootTestApplication {
 
 	public static void main(String[] args) {
