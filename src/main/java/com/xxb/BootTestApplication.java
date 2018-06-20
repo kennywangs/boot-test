e499329cc91f5f22de902049695b17445da6e2ec
Administrator <Administrator@hx-pc>
2018-06-21 03:06:11 +0800

    properties

--- src/main/java/com/xxb/BootTestApplication.java
+++ src/main/java/com/xxb/BootTestApplication.java
@@ -1,13 +1,13 @@
 package com.xxb;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.xxb.config.LoadAdditionalProperties;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = { "com.xxb.**.config","com.xxb.**.service" })
@@ -16,10 +16,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class BootTestApplication {
 	
-	private static final Logger logger = LoggerFactory.getLogger(BootTestApplication.class);
-
 	public static void main(String[] args) {
-		SpringApplication.run(BootTestApplication.class, args);
+		new SpringApplicationBuilder(BootTestApplication.class).listeners(new LoadAdditionalProperties()).run(args);
 	}
 
 }
---
+++ src/main/java/com/xxb/config/LoadAdditionalProperties.java
@@ -0,0 +1,31 @@
+package com.xxb.config;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.env.PropertiesPropertySourceLoader;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+public class LoadAdditionalProperties implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+	private ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+	@Override
+	public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+		try {
+			Resource[] resources = (Resource[]) resolver.getResources("classpath:project-*.properties");
+			for (Resource resource:resources) {
+				List<PropertySource<?>> propertySources = new PropertiesPropertySourceLoader().load(resource.getFilename(), resource);
+				for (PropertySource<?> ps:propertySources) {
+					event.getEnvironment().getPropertySources().addLast(ps);
+				}
+			}
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+}
