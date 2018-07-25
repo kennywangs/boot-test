d05f80cb437183d2c7f3e101b21960de69981c5c
kennywangs <wangke414@163.com>
2018-07-25 17:58:06 +0800

    init sql default timezone

---
+++ init.sql
@@ -0,0 +1,4 @@
+INSERT INTO "id_authority" ("id", "auth_type", "auth_url", "create_date", "description", "modify_date", "name", "need_auth") VALUES ('f2852721-58df-456e-99bc-7bba1d6a3e4c', 10, '/auth/**', '2018-06-20 15:53:39.708', '授权权限', '2018-06-20 15:53:39.708', 'authurl', 'true');
+INSERT INTO "id_authority" ("id", "auth_type", "auth_url", "create_date", "description", "modify_date", "name", "need_auth") VALUES ('31c74782-b4fd-445c-ba28-34aa537a4d64', 10, '/user/**', '2018-06-21 16:51:36.426', '用户权限', '2018-06-21 16:51:36.426', 'userurl', 'true');
+INSERT INTO "id_role" ("id", "create_date", "description", "modify_date", "name", "role_type") VALUES ('800c9956-b35b-471d-92d7-ee1911f5454c', '2018-06-20 15:38:40.569', '系统管理员', '2018-06-20 15:38:40.569', 'admin', 10);
+
--- src/main/java/com/xxb/BootTestApplication.java
+++ src/main/java/com/xxb/BootTestApplication.java
@@ -1,8 +1,10 @@
 package com.xxb;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,12 +12,17 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import com.xxb.config.LoadAdditionalProperties;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = { "com.xxb.**.config","com.xxb.**.service" })
+// @ComponentScan(basePackages = { "com.xxb.**.config","com.xxb.**.service" })
 @ComponentScan(basePackages = { "com.xxb" })
-//@EnableAutoConfiguration(exclude = { MultipartAutoConfiguration.class})
+// @EnableAutoConfiguration(exclude = { MultipartAutoConfiguration.class})
 @EnableTransactionManagement
 public class BootTestApplication {
-	
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+	}
+
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(BootTestApplication.class).listeners(new LoadAdditionalProperties()).run(args);
 	}
